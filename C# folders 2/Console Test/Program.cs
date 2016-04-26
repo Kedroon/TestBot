dc8280db8a90c706970893881909340d6447d1cd
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2016-04-26 13:59:42 -0400

    Added files via upload

---
+++ C# folders 2/Console Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperTerminais superterminais = new SuperTerminais();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            superterminais.StartAnalysis();
+            watch.Stop();
+            Console.WriteLine("Execution Time: " + (watch.ElapsedMilliseconds) + "Ms");
+            Console.Read();
+        }
+    }
+}
